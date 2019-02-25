184ed4a4c76043a590e41e52b4a232724cca05b9
alonsolee <lizhenchang@gmail.com>
2019-02-25 16:19:16 +0800

    bug fixed

---
+++ src/FastWechat/Aligado.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lizhenchang
+ * Date: 2019/2/25
+ * Time: 4:14 PM
+ */
+
+namespace Alonsolee\FastWechat;
+
+
+class Aligado
+{
+	public function hello(){
+		return 'alonso';
+	}
+}
--- src/Test.php
+++ src/Test.php
@@ -11,15 +11,19 @@ namespace Alonsolee\FastWeChat;
 
 
 
+use EasyWeChatComposer\EasyWeChat;
 use Overtrue\Socialite\AccessToken;
 use Overtrue\Socialite\User;
 use PHPUnit\Framework\TestCase;
+require __DIR__.'/../vendor/autoload.php';
 
 class Test extends TestCase
 {
 	public function testJsonserialize()
 	{
-		print_r('hello moto');
+//		print_r('hello moto');
+		print_r((new Aligado())->hello());
+		$l = new ServiceProvider(null);
 //		$this->assertSame('[]', json_encode(new User([])));
 
 //		$this->assertSame('{"token":"mock-token"}', json_encode(new User(['token' => new AccessToken(['access_token' => 'mock-token'])])));
